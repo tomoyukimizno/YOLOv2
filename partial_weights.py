db46850209d5a4b53af143cd49debc1d78bfb964
adusa1019 <symphony20030829@yahoo.co.jp>
2017-07-11 16:14:41 +0900

    不要なimportを削除

--- partial_weights.py
+++ partial_weights.py
@@ -6,8 +6,6 @@ import chainer.functions as F
 import argparse
 from darknet19 import *
 from yolov2 import *
-from yolov2_grid_prob import *
-from yolov2_bbox import *
 
 n_classes = 10
 n_boxes = 5
